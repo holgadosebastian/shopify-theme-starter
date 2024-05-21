46359784a7299c7ca081a784dda9de1db3a5cc5a
Sebastian Holgado <holgado.sebastian@gmail.com>
2024-05-21 21:52:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const path = require('path');
 const fs = require('node:fs');
 const yargs = require("yargs");
 const { execSync } = require('child_process');
+const readline = require('readline-sync'); 
 
 console.log('process.argv', process.argv);
 console.log(yargs.argv);
 
 const currentPath = process.cwd();
 
-function createPackageJson(currentPath, { storeUrl }) {
+function createPackageJson(currentPath}) {
+  const storeUrl = readline.question("Enter store myshopify url: ");
+  console.log(storeUrl);
+  
   let packageJson;
   try {
     packageJson = require(path.join(currentPath, 'package.json'));
@@ -44,7 +48,7 @@ function main() {
     if (!storeUrl) throw Error('store url has not been defined, use --store-url={myshopify.com url}');
     createPackageJson(currentPath, { storeUrl});
   } catch (error) {
-    console.log('error', error);
+    console.error(error);
     process.exit(1);
   }
 }
