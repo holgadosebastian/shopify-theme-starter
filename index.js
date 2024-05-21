a13bf76a53ace812af946234afd87dffbe0f8f7a
Sebastian Holgado <holgado.sebastian@gmail.com>
2024-05-21 21:36:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,41 @@
 const path = require('path');
 const fs = require('node:fs');
 const yargs = require("yargs");
-const packageJson = require('./package.json');
+const { execSync } = require('child_process');
 
 console.log('process.argv', process.argv);
 console.log(yargs.argv);
 
 const currentPath = process.cwd();
-const {storeUrl} = yargs.argv;
 
-packageJson.scripts = {
-  'theme:dev': `shopify dev --store=${storeUrl}`,
-  'theme:push': `shopify push --store=${storeUrl}`,
-};
+try {
+  await createPackageJson(currentPath)
+} catch (error) {
+  console.log('error')
+  process.exit(1);
+}
 
-packageJson.name = storeUrl;
-console.log("Current directory:", (path.resolve(__dirname, 'package2.json')));
+async function createPackageJson(currentPath) {
+  const packageJson = require(path.join(currentPath, 'package.json')) || {};
+  const resolvedFileName = path.join(currentPath, 'package2.json');
+  const {storeUrl} = yargs.argv;
 
-const resolvedFileName = path.join(currentPath, 'package2.json');
+  console.log('packageJson', packageJson);
 
-fs.writeFile(resolvedFileName, JSON.stringify(packageJson, null, 2), err => {
-  if (err) {
-    console.error(err);
-  } else {
-    // file written successfully
-  }
-});
+  packageJson.scripts = {
+    'theme:dev': `shopify dev --store=${storeUrl}`,
+    'theme:push': `shopify push --store=${storeUrl}`,
+  };
+  
+  packageJson.name = storeUrl;
+  
+  fs.writeFile(resolvedFileName, JSON.stringify(packageJson, null, 2), err => {
+    if (err) {
+      console.error(err);
+    } else {
+      // file written successfully
+    }
+  });
+
+  
+}
